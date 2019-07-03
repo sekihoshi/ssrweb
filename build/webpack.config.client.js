342c9bc682720abd179efd8e6c67583e19c8826a
shixing <shixing@vrvmail.com.cn>
2019-07-03 17:06:56 +0800

    dev 开发环境前端配置webpack-dev-server

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 
+let isDev = process.env.NODE_ENV == "devlopment"
+
 let config = {
     entry: {
         app: path.join(__dirname, '../client/index.js'),
@@ -25,6 +27,18 @@ let config = {
             template: path.join(__dirname, '../client/template.html')
         })
     ]
+}
+
+if(isDev) {
+    config.devServer = {
+        host: '0.0.0.0',
+        port: 3000,
+        publicPath: '/public/',
+        contentBase: path.join(__dirname,'../dist'),
+        historyApiFallback: {
+            index: '/public/index.html'
+        }
+    }
 }
 
 module.exports = config
