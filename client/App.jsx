2701ae3fbe4c837a0615b6910e3131dffcb0caa4
shixing <shixing@vrvmail.com.cn>
2019-07-08 16:19:48 +0800

    引入 antd list组件

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
 import Header from './component/header'
-
+import ArticleList from './component/articleList';
 class App extends React.PureComponent {
     render () {
         return <div>
             <Header />
+            <ArticleList />
         </div>
     }
 }
