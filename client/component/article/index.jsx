432b3e164481d49e4c61159fdfe425bb3de1d59d
sekihoshi <992670404@qq.com>
2019-07-11 22:48:01 +0800

    文章标题

--- client/component/article/index.jsx
+++ client/component/article/index.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { Breadcrumb } from 'antd'
-import style from './index.css'
-
+import React from "react";
+import { Breadcrumb, Row, Col } from "antd";
+import style from "./index.css";
 
 export default class extends React.PureComponent {
-    state = {}
+  state = {};
 
-    render() {
-        return(
-            <React.Fragment>
-                <div className={style.article}>
-
-                </div>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className={style.article}>
+          <Row>
+            <Col span={24}>
+              <Breadcrumb>
+                <Breadcrumb.Item>首页</Breadcrumb.Item>
+                <Breadcrumb.Item>分类</Breadcrumb.Item>
+                <Breadcrumb.Item>正文</Breadcrumb.Item>
+              </Breadcrumb>
+            </Col>
+          </Row>
+          <Row>
+            <h1>This is a artcle title and its right is h1</h1>
+            <div className={}></div>
+          </Row>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
