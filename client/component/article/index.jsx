3c0c1692f4010485c06fa9e6a83b8a106c52bfad
shixing <shixing@vrvmail.com.cn>
2019-07-11 18:46:52 +0800

    增加文章详情页(未完成)

---
+++ client/component/article/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Breadcrumb } from 'antd'
+import style from './index.css'
+
+
+export default class extends React.PureComponent {
+    state = {}
+
+    render() {
+        return(
+            <React.Fragment>
+                <div className={style.article}>
+
+                </div>
+            </React.Fragment>
+        )
+    }
+}
